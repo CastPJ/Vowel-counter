543a968307f9985fa8d70027ac23fe2b8044c8fe
CastPJ <JPajak.dev@gmail.com>
2024-07-08 08:37:55 +0200

    Updated visuals

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const word = document.getElementById("word");
+const input = document.getElementById("word");
 const submitButton = document.getElementById("submit-button");
 
 submitButton.addEventListener("click", output);
 
 function output(e) {
   e.preventDefault();
-  console.log(word.value);
+  const word = input.value.toString();
+  console.log(word);
 }
